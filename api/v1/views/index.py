64ce278324781982bf762299984d9101e1cd2a34
AymanAegon <ayo12ammar24@gmail.comnode>
2023-10-28 20:53:03 +0100

    fic

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -32,10 +32,11 @@ def api_stats():
         return the number of objects
     """
     dic = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")}
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
     return jsonify(dic)
