ee37761f9055d7f4454faa924be9b4ddb722321d
AymanAegon <ayo12ammar24@gmail.comnode>
2023-10-28 21:03:48 +0100

    5

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 app for registering blueprint and starting flask
 '''
 from flask import Flask
-from flask import jsonify
+from flask import jsonify, make_response
 from api.v1.views import app_views
 from models import storage
 from os import getenv
@@ -21,7 +21,7 @@ def teardown_db(exception):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
