f501cb7bf17f36c0dca81d7f9d89f207e45299d0
AymanAegon <ayo12ammar24@gmail.comnode>
2023-10-28 20:11:59 +0100

    redo

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-# Import the index route to register it with the Blueprint
-from api.v1.views import index
 from flask import Blueprint
 
 # Create a Blueprint object with a URL prefix
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Import the index route to register it with the Blueprint
+from api.v1.views import index
