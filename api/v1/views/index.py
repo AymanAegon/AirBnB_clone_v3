8ce1bc1ea546f73aaf50ed007d7ee9fc6264312c
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2023-10-28 20:41:17 +0100

    de

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@ views
 """
 from . import app_views
 from flask import jsonify
+from models import storage
+
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
 
 
 # Define a route for /status
@@ -12,4 +18,15 @@ def api_status():
     """
     Return the status of your API
     """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats',methods=['GET'], strict_slashes=False)
+def api_stats():
+    """
+        return the number of objects
+    """
+    dict={}
+    for key,value in classes.items():
+        dict[key] = value.count()
     return jsonify({"status": "OK"})
