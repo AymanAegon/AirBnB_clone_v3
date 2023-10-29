5aa8d7d41ef708c04bf91847fc088be358ea8afb
AymanAegon <ayo12ammar24@gmail.comnode>
2023-10-29 18:42:19 +0100

    12

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,6 @@ CORS(app, origins="0.0.0.0")
 
 # Register Blueprint
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 # Teardown app context to close the database connection
